8d5c3275cd2ed67291b5ad82af278cb12ca8288f
Tur-Ochir11 <turochir11@gmail.com>
2025-12-09 18:29:41 +0800

    move

--- Assets/Scripts/NetworkPlayerMovement.cs
+++ Assets/Scripts/NetworkPlayerMovement.cs
@@ -6,8 +6,25 @@ public class NetworkPlayerMovement : NetworkBehaviour
 {
     public float speed = 5f;
 
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
+        if (!IsOwner) return;
         
+        var input = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        Move(input);
+    }
+
+    public void Move(Vector2 direction)
+    {
+        if (direction.sqrMagnitude < 0.01f) return;
+        
+        rb.AddForce( direction * speed, ForceMode2D.Impulse);
     }
 }
