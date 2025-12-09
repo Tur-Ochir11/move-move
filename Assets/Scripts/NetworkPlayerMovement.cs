df738da9a7318e1ad2b2f50490f8a1b5e6f63b85
Tur-Ochir11 <turochir11@gmail.com>
2025-12-09 18:06:50 +0800

    project setup

---
+++ Assets/Scripts/NetworkPlayerMovement.cs
@@ -0,0 +1,13 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkPlayerMovement : NetworkBehaviour
+{
+    public float speed = 5f;
+
+    private void Update()
+    {
+        
+    }
+}
