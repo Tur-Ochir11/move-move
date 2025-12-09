1dcf813124a8df11bc0129266a91a7343e8e3a31
Tur-Ochir11 <turochir11@gmail.com>
2025-12-09 19:21:17 +0800

    scene main

--- Assets/Scripts/NetworkPlayerMovement.cs
+++ Assets/Scripts/NetworkPlayerMovement.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class NetworkPlayerMovement : NetworkBehaviour
 {
+    [Header("Movement")]
     public float speed = 5f;
+    public float jumpForce = 5f;
+    public float acceleration = 0.5f;
+    public float braking = 0.5f;
+    [Header("Ground Check")]
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] float groundCheckDistance = 0.1f;
 
     private Rigidbody2D rb;
 
@@ -19,12 +26,47 @@ public class NetworkPlayerMovement : NetworkBehaviour
         
         var input = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
         Move(input);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
     }
 
-    public void Move(Vector2 direction)
+    public void Move(Vector2 input)
     {
-        if (direction.sqrMagnitude < 0.01f) return;
-        
-        rb.AddForce( direction * speed, ForceMode2D.Impulse);
+        float targetSpeed = input.x * speed;
+
+        float accelRate = Mathf.Abs(input.x) > 0.01f
+            ? acceleration
+            : braking; // 👈 braking when no input
+
+        float newX = Mathf.MoveTowards(
+            rb.linearVelocity.x,
+            targetSpeed,
+            accelRate * Time.fixedDeltaTime
+        );
+
+        rb.linearVelocity = new Vector2(newX, rb.linearVelocity.y);
     }
+
+
+    public void Jump()
+    {
+        if (!IsGrounded()) return;
+        
+        rb.AddForceY(jumpForce, ForceMode2D.Impulse);
+    }
+    
+
+    bool IsGrounded()
+    {
+        return Physics2D.Raycast(
+            transform.position,
+            Vector2.down,
+            groundCheckDistance,
+            groundMask
+        );
+    }
+
 }
